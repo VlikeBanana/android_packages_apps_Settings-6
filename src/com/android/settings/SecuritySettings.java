3666f7cf2567856078fc872d05affe0aa1f82ea6
Chintan Shah <cgintan1998@gmail.com>
2014-02-10 18:03:47 -0700

    [2/2] Lockscreen See through - Thanks PA

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -78,6 +78,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private static final String KEY_OWNER_INFO_SETTINGS = "owner_info_settings";
     private static final String KEY_ENABLE_WIDGETS = "keyguard_enable_widgets";
     private static final String PREF_LOCK_SCREEN = "lock_screen_settings";
+    private static final String KEY_SEE_TRHOUGH = "see_through";
 
     private static final int SET_OR_CHANGE_LOCK_METHOD_REQUEST = 123;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_IMPROVE_REQUEST = 124;
@@ -139,6 +140,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private CheckBoxPreference mMenuUnlock;
     private ListPreference mSlideLockTimeoutDelay;
     private ListPreference mSlideLockScreenOffDelay;
+    private CheckBoxPreference mSeeThrough;
 
 
     public SecuritySettings() {
@@ -331,6 +333,9 @@ public class SecuritySettings extends RestrictedSettingsFragment
                 break;
             }
         }
+
+        // lockscreen see through
+        mSeeThrough = (CheckBoxPreference) root.findPreference(KEY_SEE_TRHOUGH);
 
         // biometric weak liveliness
         mBiometricWeakLiveliness =
@@ -786,6 +791,9 @@ public class SecuritySettings extends RestrictedSettingsFragment
             } else {
                 setNonMarketAppsAllowed(false);
             }
+        } else if (preference == mSeeThrough) {
+            Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_SEE_THROUGH,
+                    mSeeThrough.isChecked() ? 1 : 0);
         } else if (KEY_TOGGLE_VERIFY_APPLICATIONS.equals(key)) {
             Settings.Global.putInt(getContentResolver(), Settings.Global.PACKAGE_VERIFIER_ENABLE,
                     mToggleVerifyApps.isChecked() ? 1 : 0);
