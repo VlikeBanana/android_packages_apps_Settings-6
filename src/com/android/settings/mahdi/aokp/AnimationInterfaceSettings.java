a6fa31aeb84f0d4b5adeb9cffe15fcc91617ac7b
Chintan Shah <cgintan1998@gmail.com>
2014-02-27 10:44:48 -0700

    [2/2] Toast Animatons - Thanks to AOSB and XuiMod

--- src/com/android/settings/mahdi/aokp/AnimationInterfaceSettings.java
+++ src/com/android/settings/mahdi/aokp/AnimationInterfaceSettings.java
@@ -37,6 +37,10 @@ public class AnimationInterfaceSettings extends SettingsPreferenceFragment imple
 
     private static final String KEY_POWER_CRT_MODE = "system_power_crt_mode";
 
+    private static final String KEY_TOAST_ANIMATION = "toast_animation";
+
+    private ListPreference mToastAnimation;
+
     private ListPreference mCrtMode; 
 
     @Override
@@ -56,6 +60,12 @@ public class AnimationInterfaceSettings extends SettingsPreferenceFragment imple
             mCrtMode.setOnPreferenceChangeListener(this);
         }
 
+        mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
+        mToastAnimation.setSummary(mToastAnimation.getEntry());
+        int CurrentToastAnimation = Settings.System.getInt(getContentResolver(), Settings.System.ACTIVITY_ANIMATION_CONTROLS[10], 1);
+        mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
+        mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
+        mToastAnimation.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -74,6 +84,14 @@ public class AnimationInterfaceSettings extends SettingsPreferenceFragment imple
                     value);
             mCrtMode.setSummary(mCrtMode.getEntries()[index]);
         }
-        return true;
+
+        if (preference == mToastAnimation) {
+             int index = mToastAnimation.findIndexOfValue((String) newValue);
+             Settings.System.putString(getContentResolver(), Settings.System.ACTIVITY_ANIMATION_CONTROLS[10], (String) newValue);
+             mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
+             Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
+             return true;
+        }
+        return false;
     }
 }
