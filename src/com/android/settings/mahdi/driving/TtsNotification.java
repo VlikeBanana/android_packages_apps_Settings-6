75a4510f190cfdf235f3bfbda412f60e369c5aa5
Chintan Shah <cgintan1998@gmail.com>
2014-03-09 15:34:32 -0600

    Fix crash

--- src/com/android/settings/mahdi/driving/TtsNotification.java
+++ src/com/android/settings/mahdi/driving/TtsNotification.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.driving;
+package com.android.settings.mahdi.driving;
 
 import android.app.Activity;
 import android.app.AlertDialog;
