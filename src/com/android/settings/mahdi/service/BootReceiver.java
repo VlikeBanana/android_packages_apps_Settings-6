fe24be727fbc827d5652af73f0b59881c6c8ad4c
Lars Greiss <kufikugel@googlemail.com>
2014-03-26 21:19:12 +0100

    Settings: QuierHours service: call with qualified user and introduce controller
    This commit fixes 2 issues:
    
    1. Call the service with a qualified user.
    
    2. Complete overwork of the handling. Currently the SmsCallHellper is doing
       way to much work. Especially when time change broadcasts trigger the service
       update. Which ended in certain cases to a complete system overload.
       To fix it we use now a single tone controller to take care of this all
    
    Change-Id: Iee4c988f206c9efe90c06114a9030637ba5bac97
    
    Conflicts:
    
    	src/com/android/settings/mahdi/service/BootReceiver.java

--- src/com/android/settings/mahdi/service/BootReceiver.java
+++ src/com/android/settings/mahdi/service/BootReceiver.java
@@ -26,10 +26,10 @@ public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        SmsCallHelper.scheduleService(context);
 
         if (BatterySaverHelper.deviceSupportsMobileData(context)) {
                 BatterySaverHelper.scheduleService(context);
+                SmsCallController.getInstance(context).scheduleService();
         }
     }
 }
