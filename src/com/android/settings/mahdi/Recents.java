547aafff4aa9ae22bbfa5324d2f36aaf5c6ebb55
Chintan Shah <cgintan1998@gmail.com>
2014-04-16 17:34:27 -0600

    [2/2] SlimRecents chooser
    Sorry that the commit doesn't have codex-corp(AOSB) has author but I will
    lke to give credits to him and Mahdi since I used their code for this :)
    Thanks
    Enjoy:D
    
    Signed-off-by: Chintan Shah <cgintan1998@gmail.com>

--- src/com/android/settings/mahdi/Recents.java
+++ src/com/android/settings/mahdi/Recents.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2014 The Schism Project
+ *  Copyright (C) 2013 The OmniROM Project
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,17 +36,25 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
+import com.android.settings.mahdi.util.Helpers;
+
 public class Recents extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "Recents";
 
+    private static final String CUSTOM_RECENT_MODE = "custom_recent_mode";
     private static final String RECENT_PANEL_LEFTY_MODE = "recent_panel_lefty_mode";
     private static final String RECENT_PANEL_SCALE = "recent_panel_scale";
     private static final String RECENT_PANEL_EXPANDED_MODE = "recent_panel_expanded_mode";
+    private static final String RECENT_MENU_CLEAR_ALL = "recent_menu_clear_all";
+    private static final String RECENT_MENU_CLEAR_ALL_LOCATION = "recent_menu_clear_all_location";
 
+    private CheckBoxPreference mRecentsCustom;
     private CheckBoxPreference mRecentPanelLeftyMode;
     private ListPreference mRecentPanelScale;
     private ListPreference mRecentPanelExpandedMode;
+    private CheckBoxPreference mRecentClearAll;
+    private ListPreference mRecentClearAllPosition;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,6 +63,12 @@ public class Recents extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
+
+        boolean enableRecentsCustom = Settings.System.getBoolean(getActivity().getContentResolver(),
+                                      Settings.System.CUSTOM_RECENT, false);
+        mRecentsCustom = (CheckBoxPreference) findPreference(CUSTOM_RECENT_MODE);
+        mRecentsCustom.setChecked(enableRecentsCustom);
+        mRecentsCustom.setOnPreferenceChangeListener(this);
 
         mRecentPanelLeftyMode = (CheckBoxPreference) findPreference(RECENT_PANEL_LEFTY_MODE);
         mRecentPanelLeftyMode.setOnPreferenceChangeListener(this);
@@ -66,14 +80,38 @@ public class Recents extends SettingsPreferenceFragment implements
         }
         mRecentPanelScale.setOnPreferenceChangeListener(this);
 
-        // i have no idea wat im doin
         mRecentPanelExpandedMode = (ListPreference) findPreference(RECENT_PANEL_EXPANDED_MODE);
         String recentPanelExpandedMode = Settings.System.getString(resolver, Settings.System.RECENT_PANEL_EXPANDED_MODE);
         if (recentPanelExpandedMode != null) {
-            mRecentPanelExpandedMode.setValue(recentPanelExpandedMode + "");
+            mRecentPanelExpandedMode.setValue(recentPanelScale);
         }
         mRecentPanelExpandedMode.setOnPreferenceChangeListener(this);
 
+        mRecentClearAll = (CheckBoxPreference) prefSet.findPreference(RECENT_MENU_CLEAR_ALL);
+        mRecentClearAll.setChecked(Settings.System.getInt(resolver,
+                Settings.System.SHOW_CLEAR_RECENTS_BUTTON, 0) == 1);
+        mRecentClearAll.setOnPreferenceChangeListener(this);
+        mRecentClearAllPosition = (ListPreference) prefSet.findPreference(RECENT_MENU_CLEAR_ALL_LOCATION);
+        String recentClearAllPosition = Settings.System.getString(resolver, Settings.System.CLEAR_RECENTS_BUTTON_LOCATION);
+        if (recentClearAllPosition != null) {
+            mRecentClearAllPosition.setValue(recentClearAllPosition);
+        }
+        mRecentClearAllPosition.setOnPreferenceChangeListener(this);
+    }
+
+    private void updatePreference() {
+        boolean customRecent = Settings.System.getBoolean(getActivity().getContentResolver(),
+                Settings.System.CUSTOM_RECENT, false);
+
+        if (customRecent == false) {
+            mRecentPanelLeftyMode.setEnabled(false);
+            mRecentPanelScale.setEnabled(false);
+            mRecentPanelExpandedMode.setEnabled(false);
+        } else {
+            mRecentPanelLeftyMode.setEnabled(true);
+            mRecentPanelScale.setEnabled(true);
+            mRecentPanelExpandedMode.setEnabled(true);
+        }
     }
 
     @Override
@@ -83,7 +121,22 @@ public class Recents extends SettingsPreferenceFragment implements
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mRecentPanelScale) {
+        if (preference == mRecentClearAll) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver, Settings.System.SHOW_CLEAR_RECENTS_BUTTON, value ? 1 : 0);
+            return true;
+        } else if (preference == mRecentClearAllPosition) {
+            String value = (String) newValue;
+            Settings.System.putString(resolver, Settings.System.CLEAR_RECENTS_BUTTON_LOCATION, value);
+            return true;
+        } else if (preference == mRecentsCustom) { // Enable||disable Slim Recent
+            Settings.System.putBoolean(resolver,
+                    Settings.System.CUSTOM_RECENT,
+                    ((Boolean) newValue) ? true : false);
+            updatePreference();
+            Helpers.restartSystemUI();
+            return true;
+        } else if (preference == mRecentPanelScale) {
             int value = Integer.parseInt((String) newValue);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.RECENT_PANEL_SCALE_FACTOR, value);
