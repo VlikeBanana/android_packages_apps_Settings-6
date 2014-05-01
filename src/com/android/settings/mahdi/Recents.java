62794a932704256181dc6e9f6594603dff0e4332
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-05-01 23:28:05 +0100

    RecentsPanelSettings: fix updating settings
    Conflicts:
    
    	src/com/android/settings/mahdi/Recents.java

--- src/com/android/settings/mahdi/Recents.java
+++ src/com/android/settings/mahdi/Recents.java
@@ -91,6 +91,7 @@ public class Recents extends SettingsPreferenceFragment implements
         mRecentClearAll.setChecked(Settings.System.getInt(resolver,
                 Settings.System.SHOW_CLEAR_RECENTS_BUTTON, 0) == 1);
         mRecentClearAll.setOnPreferenceChangeListener(this);
+
         mRecentClearAllPosition = (ListPreference) prefSet.findPreference(RECENT_MENU_CLEAR_ALL_LOCATION);
         String recentClearAllPosition = Settings.System.getString(resolver, Settings.System.CLEAR_RECENTS_BUTTON_LOCATION);
         if (recentClearAllPosition != null) {
