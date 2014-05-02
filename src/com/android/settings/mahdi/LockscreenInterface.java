900e405b543f5f3055ad0db1ba44db7d93283d88
Cristiano Matos <cristianobmatos@gmail.com>
2014-05-02 12:23:35 -0300

    PEEK: configurable pickup timeout (1/2)
    - Allow the user to set different pickup timeout when a notification comes up through PEEK.
    Options:
    15 sec (default)
    30 sec
    45 sec
    1 min
    2 min
    
    Change-Id: I82650f376b1dee81b5872317c70506d580361d49
    
    Conflicts:
    
    	res/values/mahdi_arrays.xml
    	res/values/mahdi_strings.xml
    	src/com/android/settings/mahdi/LockscreenInterface.java

--- src/com/android/settings/mahdi/LockscreenInterface.java
+++ src/com/android/settings/mahdi/LockscreenInterface.java
@@ -28,6 +28,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -62,6 +63,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
     private static final String KEY_BATTERY_STATUS = "lockscreen_battery_status";
     private static final String KEY_LOCKSCREEN_BUTTONS = "lockscreen_buttons";
     private static final String KEY_LOCKSCREEN_NOTIFICATONS = "lockscreen_notifications";
+    private static final String KEY_PEEK_PICKUP_TIMEOUT = "peek_pickup_timeout";
     private static final String LOCKSCREEN_SHORTCUTS_CATEGORY = "lockscreen_shortcuts_category";
     private static final String PREF_LOCKSCREEN_EIGHT_TARGETS = "lockscreen_eight_targets";
     private static final String PREF_LOCKSCREEN_TORCH = "lockscreen_glowpad_torch";
@@ -69,6 +71,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
 
     private PreferenceCategory mAdditionalOptions;
     private LockscreenNotificationsPreference mLockscreenNotifications;
+    private ListPreference mPeekPickupTimeout;
     private ListPreference mBatteryStatus;
     private CheckBoxPreference mLockscreenEightTargets;
     private CheckBoxPreference mGlowpadTorch;
@@ -106,6 +109,13 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
                 prefs.findPreference(KEY_ADDITIONAL_OPTIONS);
 
         mLockscreenNotifications = (LockscreenNotificationsPreference) findPreference(KEY_LOCKSCREEN_NOTIFICATONS);
+
+        mPeekPickupTimeout = (ListPreference) prefs.findPreference(KEY_PEEK_PICKUP_TIMEOUT);
+        int peekTimeout = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.PEEK_PICKUP_TIMEOUT, 0, UserHandle.USER_CURRENT);
+        mPeekPickupTimeout.setValue(String.valueOf(peekTimeout));
+        mPeekPickupTimeout.setSummary(mPeekPickupTimeout.getEntry());
+        mPeekPickupTimeout.setOnPreferenceChangeListener(this);
 
         mBatteryStatus = (ListPreference) findPreference(KEY_BATTERY_STATUS);
         if (mBatteryStatus != null) {
@@ -198,8 +208,23 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         } else if (preference == mLockscreenEightTargets) {
             showDialogInner(DLG_ENABLE_EIGHT_TARGETS, (Boolean) objValue);
             return true;
+        } else if (preference == mPeekPickupTimeout) {
+            int peekTimeout = Integer.valueOf((String) objValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                Settings.System.PEEK_PICKUP_TIMEOUT,
+                    peekTimeout, UserHandle.USER_CURRENT);
+            updatePeekTimeoutOptions(objValue);
+            return true;
         }
         return false;
+    }
+
+    private void updatePeekTimeoutOptions(Object newValue) {
+        int index = mPeekPickupTimeout.findIndexOfValue((String) newValue);
+        int value = Integer.valueOf((String) newValue);
+        Settings.Secure.putInt(getActivity().getContentResolver(),
+                Settings.System.PEEK_PICKUP_TIMEOUT, value);
+        mPeekPickupTimeout.setSummary(mPeekPickupTimeout.getEntries()[index]);
     }
 
     private void showDialogInner(int id, boolean state) {
