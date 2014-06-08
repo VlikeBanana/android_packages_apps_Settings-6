b4a3bcb0c0418e5a2657a358843efdad7853ff11
Valter Strods <valters.strods@gmail.com>
2014-06-08 03:45:25 +0300

    Settings: Silently update the package manager
    This commit implements automatic updating of the "Choose application"
    list used for creating configurations for new apps. The list is now
    being updated each time an app is either added to or removed from the
    existing custom configurations.
    
    Change-Id: I2d442fb9e573e65bf686a48ed15b5479d4238d4d

--- src/com/android/settings/mahdi/notificationlight/NotificationLightSettings.java
+++ src/com/android/settings/mahdi/notificationlight/NotificationLightSettings.java
@@ -300,6 +300,8 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
                 }
             }
         }
+
+        mPackageAdapter.reloadList();
     }
 
     private void setCustomEnabled() {
@@ -600,7 +602,7 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
     private class PackageAdapter extends BaseAdapter {
         private List<PackageItem> mInstalledPackages = new LinkedList<PackageItem>();
 
-        private void reloadList() {
+        public void reloadList() {
             final Handler handler = new Handler();
             new Thread(new Runnable() {
                 @Override
